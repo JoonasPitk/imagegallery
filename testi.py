5dac9d6cd7fe616ac2ce97c9912ca9050e712798
JoonasPitk <46161@edu.raseko.fi>
2023-01-23 15:43:12 +0200

    Add personal icons, and minor touches.

--- testi.py
+++ testi.py
@@ -1,18 +1,25 @@
+import sys
+import pathlib
+
 from PyQt5.QtWidgets import QApplication
 from pyqt_slideshow import SlideShow
 
+module_path = pathlib.Path(__file__).parent.absolute()
+icon_path = module_path / "icon"
 
 if __name__ == "__main__":
-    import sys
     app = QApplication(sys.argv)
-    s = SlideShow()
-    s.setFilenames([
+    slideshow = SlideShow()
+    slideshow.setFilenames([
         "./testpictures/AlbumArt1.jpg",
         "./testpictures/AlbumArt2.jpg",
         "./testpictures/AlbumArt3.jpg",
         "./testpictures/AlbumArt4.jpg",
         "./testpictures/AlbumArt5.jpg",])
-    # s.setNavigationButtonVisible(False) # to not show the navigation button
-    # s.setBottomButtonVisible(False) # to not show the bottom button
-    s.show()
+    # slideshow.setNavigationButtonVisible(False) # Do not show left and right navigation buttons.
+    # slideshow.setBottomButtonVisible(False) # Do not show bottom navigation buttons.
+    slideshow.setTimerEnabled(False) # Disable the slideshow timer when booted up.
+    slideshow._SlideShow__nextBtn.setIcon(icon_path / "next.svg")
+    slideshow._SlideShow__prevBtn.setIcon(icon_path / "prev.svg")
+    slideshow.show()
     app.exec_()
