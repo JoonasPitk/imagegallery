5246b9a3792d93eff5b8892028fd6dd7b2180308
JoonasPitk <46161@edu.raseko.fi>
2023-03-20 17:40:04 +0200

    Always respect timer state, plus rearrange code.

--- adjustments.py
+++ adjustments.py
@@ -1,15 +1,41 @@
-from PyQt5.QtCore import Qt
+from pathlib import Path
 
+from PyQt5.QtCore import Qt
 from pyqt_slideshow import SlideShow
 
 
 class CustomSettings(SlideShow):
-    # For overriding some pyqt_slideshow functionalities.
+
     def _SlideShow__initUi(self, *args, **kwargs):
-        super()._SlideShow__initUi(*args, **kwargs) # Bring initUI from the library.
-        self.qtimer = self._SlideShow__timer # Set the library's timer variable to a new one.
-        self.timer = ToggleableTimer(self.qtimer) # Set ToggleableTimer class to a variable.
-        self._SlideShow__timer = self.timer # When library's timer is called, use ours instead.
+        super()._SlideShow__initUi(*args, **kwargs) # Bring __initUI from the library.
+    
+        # Replace pyqt_slideshow's timer.
+
+        self.qtimer = self._SlideShow__timer # Get the library's original timer to self.qtimer.
+        self.timer = ToggleableTimer(self.qtimer) # Create a wrapped timer from the original QTimer.
+        self._SlideShow__timer = self.timer # Replace the library's timer with ours.
+
+        # Set our own timer interval.
+        self.interval = 1000 # In milliseconds.
+        self.setInterval(self.interval)
+
+        # Disable the timer at the start.
+        self.setTimerEnabled(False)
+
+        # Keep the aspect ratio of images.
+        self._SlideShow__view.setAspectRatioMode(Qt.KeepAspectRatio)
+
+        # Set custom icons.
+
+        modulePath = Path(__file__).parent.absolute()
+        iconPath = modulePath / "icon"
+        self._SlideShow__nextBtn.setIcon(iconPath / "next.svg")
+        self._SlideShow__prevBtn.setIcon(iconPath / "prev.svg")
+
+        # self.setNavigationButtonVisible(False) # Do not show left and right navigation buttons.
+        # self.setBottomButtonVisible(False) # Do not show bottom navigation buttons.
+
+        # TODO: Set the window aspect ratio to 16:9, not by the first loaded picture?
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Q:
@@ -18,26 +44,16 @@ class CustomSettings(SlideShow):
             self.toggleSlideShowPlayback()
         if event.key() == Qt.Key_K:
             self._SlideShow__next()
-            # If the timer is already running, reset the countdown.
-            if self.timer.disabled == False:
-                SlideShow.setInterval(self, 1000)
+            self.setInterval(self.interval)
         if event.key() == Qt.Key_J:
-            if self.timer.disabled:
-                self._SlideShow__prev()
-                # Library starts the timer here, so we stop it.
-                self.timer.stop()
-            else:
-                self._SlideShow__prev()
-                # Timer is already running, reset the countdown.
-                SlideShow.setInterval(self, 1000)
-
-    # TODO: Respect the timer state when UI elements are clicked.
+            self._SlideShow__prev()
+            self.setInterval(self.interval)
         
     def toggleSlideShowPlayback(self):
         if self.timer.disabled:
-            self.timer.start()
+            self.timer.enable()
         else:
-            self.timer.stop()
+            self.timer.disable()
 
 
 class ToggleableTimer:
@@ -45,14 +61,23 @@ class ToggleableTimer:
         self.qtimer = qtimer
         self.disabled = True
 
-# Since we're replacing the library timer, we need to define methods it'd need.
-    def start(self):
-        self.qtimer.start()
+    def enable(self):
         self.disabled = False
+        self.start()
+
+    def disable(self):
+        self.stop()
+        self.disabled = True
+
+    # Since we're replacing the library timer, we need to define methods it'll need.
+
+    def start(self):
+        if self.disabled:
+            return
+        self.qtimer.start()
     
     def stop(self):
         self.qtimer.stop()
-        self.disabled = True
 
     def setInterval(self, value: int):
         self.qtimer.setInterval(value)
--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-from pathlib import Path
 from sys import argv
 
-from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication
 
 from customFileDialog import fileDialog
 from adjustments import CustomSettings
 
-
-modulePath = Path(__file__).parent.absolute()
-iconPath = modulePath / "icon"
 
 def main():
     app = QApplication(argv)
@@ -17,17 +12,8 @@ def main():
     if not selectedFiles:
         return
     slideShow = CustomSettings()
-    # TODO: Set the window size to 16:9, not by the first loaded picture.
     slideShow.setWindowTitle("Image Gallery")
     slideShow.setFilenames(selectedFiles)
-    # Fit an image to the window, no matter the aspect ratio.
-    slideShow._SlideShow__view.setAspectRatioMode(Qt.KeepAspectRatio)
-    # slideShow.setNavigationButtonVisible(False) # Do not show left and right navigation buttons.
-    # slideShow.setBottomButtonVisible(False) # Do not show bottom navigation buttons.
-    slideShow.setInterval(1000) # Milliseconds before moving to the next image.
-    slideShow.setTimerEnabled(False) # Disable the slide show timer for the first image.
-    slideShow._SlideShow__nextBtn.setIcon(iconPath / "next.svg")
-    slideShow._SlideShow__prevBtn.setIcon(iconPath / "prev.svg")
     slideShow.show()
     app.exec_()
 
